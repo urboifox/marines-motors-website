77891f6fa8203805725a708aa0ca0804b57f3f01
Gunblade <mohamed.dev.egy@gmail.com>
2023-03-01 05:33:57 +0200

    fixed email function

--- js/mail.js
+++ js/mail.js
@@ -1,25 +1,29 @@
 // mail JS
 
 function sendMail() {
-  let params = {
-    name: document.getElementById(`name`).value,
-    email: document.getElementById(`email`).value,
-    message: document.getElementById(`message`).value,
-    phone: document.getElementById(`phone`).value,
-    website: document.getElementById(`website`).value,
-  };
-  const serviceID = "service_0ookowh";
-  const templateID = "template_2p6i7bz";
+  if (
+    document.getElementById(`name`).value !== "" &&
+    document.getElementById(`email`).value !== ""
+  ) {
+    let params = {
+      name: document.getElementById(`name`).value,
+      email: document.getElementById(`email`).value,
+      message: document.getElementById(`message`).value,
+      phone: document.getElementById(`phone`).value,
+      country: document.getElementById(`country`).value,
+    };
+    const serviceID = "service_0ookowh";
+    const templateID = "template_2p6i7bz";
 
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      document.getElementById(`name`).value = "";
-      document.getElementById(`email`).value = "";
-      document.getElementById(`message`).value = "";
-      document.getElementById(`phone`).value = "";
-      document.getElementById(`website`).value = "";
-      console.log(res);
-    })
-    .catch((err) => console.log(err));
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById(`name`).value = "";
+        document.getElementById(`email`).value = "";
+        document.getElementById(`message`).value = "";
+        document.getElementById(`phone`).value = "";
+        document.getElementById(`country`).value = "";
+      })
+      .catch((err) => console.log(err));
+  }
 }
